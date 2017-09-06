2d831c44e2514900b2a66f734a21ac4e5e2203c2
Godfrey Chan <godfreykfc@gmail.com>
2017-09-05 17:51:40 -0700

    Make validations value-based

--- packages/core/src/descriptor.ts
+++ packages/core/src/descriptor.ts
@@ -1,11 +1,32 @@
-import { Dict, unknown } from 'ts-std';
+import { Task } from 'no-show';
+import { unknown } from 'ts-std';
 
-export type ValidationDescriptor = Readonly<{
-  validator: Readonly<{ name: string, args: ReadonlyArray<unknown> }>;
-  keys: ReadonlyArray<string>;
+export type ErrorPath = ReadonlyArray<string>;
+
+export interface ErrorMessage {
+  key: string;
+  args: unknown;
+}
+
+export interface ValidationError<Message extends ErrorMessage> {
+  path: ErrorPath;
+  message: Message;
+}
+
+export interface Environment {
+  get(object: unknown, key: string): unknown;
+}
+
+export type ValidatorFactory<Options = unknown, Message extends ErrorMessage = ErrorMessage> =
+  (env: Environment, options: Options) => Validator<Message>;
+
+export type Validator<Message extends ErrorMessage> =
+  (value: unknown) => Task<Array<ValidationError<Message>>>;
+
+export type ValidationDescriptor<Options = unknown> = Readonly<{
+  factory: ValidatorFactory<Options, ErrorMessage>;
+  options: Options;
   contexts: ReadonlyArray<string>;
 }>;
 
 export type ValidationDescriptors = ReadonlyArray<ValidationDescriptor>;
-
-export type FieldValidationDescriptors = Readonly<Dict<ValidationDescriptors>>;
