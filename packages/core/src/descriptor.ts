0465d269f33f1e7d23be4e49873409889588dd1e
Yehuda Katz <wycats@gmail.com>
2017-10-30 23:52:51 -0500

    Document the core types

--- packages/core/src/descriptor.ts
+++ packages/core/src/descriptor.ts
@@ -3,24 +3,66 @@ import { Option, unknown } from 'ts-std';
 
 export type ErrorPath = ReadonlyArray<string>;
 
+/**
+ * @api public
+ * 
+ * A represenation of a failed validation, not including its location. This object
+ * can be used together with a formatter to produce human-readable errors. The
+ * information provided in an `ErrorMessage` should be enough to properly
+ * internationalize the error message or use it in other localization contexts. 
+ */
 export interface ErrorMessage {
   key: string;
   args: unknown;
 }
 
+/**
+ * @api public
+ * 
+ * A representation of a failed validation, including the location in the validated
+ * object where the validation failed.
+ */
 export interface ValidationError {
   path: ErrorPath;
   message: ErrorMessage;
 }
 
+/**
+ * @api host
+ * 
+ * An object that provides host-specific behavior for validators. It is passed in to
+ * all `ValidatorFactory`s, so hosts can also extend Environment to communicate with
+ * validators written to be used in that environment. 
+ */
 export interface Environment {
   get(object: unknown, key: string): unknown;
 }
 
+/**
+ * @api primitive
+ * 
+ * A function that takes an environment and validator options and produces a new
+ * Validator function. In other words, it curries the environment and options.
+ */
 export type ValidatorFactory<T, Options> = (env: Environment, options: Options) => Validator<T>;
 
+/**
+ * @api primitive
+ * 
+ * A function that takes a value and a validation context and (asynchronously)
+ * produces a list of validation errors.
+ * 
+ * A successful validation produces an empty list of validation errors.
+ * 
+ * Primitive validations must use no-show Tasks (which can be cancelled) to manage asynchrony.
+ */
 export type Validator<T = unknown> = (value: T, context: Option<string>) => Task<ValidationError[]>;
 
+/**
+ * @api primitive
+ * 
+ * A low-level representation of a validation.
+ */
 export type ValidationDescriptor<T = unknown, Options = unknown> = Readonly<{
   factory: ValidatorFactory<T, Options>;
   options: Options;
--- packages/core/src/validate.ts
+++ packages/core/src/validate.ts
@@ -2,6 +2,24 @@ import { Task } from 'no-show';
 import { Option } from 'ts-std';
 import { Environment, ValidationDescriptor, ValidationError } from './descriptor';
 
+/**
+ * @api public
+ * 
+ * A function that takes an environment, value, descriptor and context, and (asynchronously)
+ * produces an array of validation errors. If this function produces an empty array, the
+ * validation succeeded.
+ * 
+ * The "context" is an app-specific concept. For example, an app might want to separate
+ * between validations that must pass when drafting an article vs. validations that
+ * must pass when publishing an article. If a validation descriptor does not specify
+ * the context passed in to `validate`, the validation passes.
+ * 
+ * @param env The host environment
+ * @param value The value to validate; it need not be an object
+ * @param descriptor A validation descriptor to use to validate the value; a single validation
+ *  descriptor can represent multiple composed validations
+ * @param context Optionally, a string that represents the saving context
+ */
 export function validate<T>(env: Environment, value: T, descriptor: ValidationDescriptor<T>, context: Option<string>): Task<ValidationError[]> {
   return new Task(async run => {
     let { factory, options, contexts } = descriptor;
