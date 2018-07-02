64b7f518a066a0b0d2acea11d2d57cff2c393ce3
Yehuda Katz <wycats@gmail.com>
2018-07-02 16:54:04 -0700

    Make yarn test work in individual packages

--- packages/schema/testem.js
+++ packages/schema/testem.js
@@ -1,25 +1,26 @@
 module.exports = {
-  "launchers": {
-    "Node": {
-      "cwd": process.env.EMBER_CLI_TEST_OUTPUT,
-      "command": `qunit "commonjs/test/**/*-test.js"`,
-      "protocol": "tap"
-    },
+  launchers: {
+    Node: {
+      cwd: process.env.EMBER_CLI_TEST_OUTPUT,
+      command: `qunit "commonjs/test/**/*-test.js"`,
+      protocol: "tap"
+    }
   },
 
-  "framework": "qunit",
-  "test_page": "index.html?hidepassed",
+  framework: "qunit",
+  test_page: "index.testem.html?hidepassed",
 
-  "browser_args": {
-    "Chrome": [
+  browser_args: {
+    Chrome: [
       "--headless",
       "--no-sandbox",
       "--disable-gpu",
-      "--remote-debugging-port=9222"
+      "--remote-debugging-port=9222",
+      "--user-data-dir=/tmp"
     ]
   },
 
-  "disable_watching": true,
-  "launch_in_dev": ["Node", "Chrome"],
-  "launch_in_ci": ["Node", "Chrome"]
-}
+  disable_watching: true,
+  launch_in_dev: ["Node", "Chrome"],
+  launch_in_ci: ["Node", "Chrome"]
+};
