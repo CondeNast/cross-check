c6007749b94874209ea753619944d17a08bcb7f3
Yehuda Katz <wycats@gmail.com>
2018-07-05 11:45:19 -0700

    Try removing the Chrome directory

--- packages/core/testem.js
+++ packages/core/testem.js
@@ -15,8 +15,7 @@ module.exports = {
       "--headless",
       "--disable-gpu",
       "--no-sandbox",
-      "--remote-debugging-port=9222",
-      "--user-data-dir=/tmp/chrome"
+      "--remote-debugging-port=9222"
     ]
   },
 
--- packages/schema/testem.js
+++ packages/schema/testem.js
@@ -15,8 +15,7 @@ module.exports = {
       "--headless",
       "--no-sandbox",
       "--disable-gpu",
-      "--remote-debugging-port=9222",
-      "--user-data-dir=/tmp/chrome"
+      "--remote-debugging-port=9222"
     ]
   },
 
