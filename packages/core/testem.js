4b90b57ee242edc80fc91b65185e8b5462668c0a
Yehuda Katz <wycats@gmail.com>
2018-07-05 11:43:53 -0700

    Try to change the Chrome profile dir

--- packages/core/testem.js
+++ packages/core/testem.js
@@ -16,7 +16,7 @@ module.exports = {
       "--disable-gpu",
       "--no-sandbox",
       "--remote-debugging-port=9222",
-      "--user-data-dir=/tmp"
+      "--user-data-dir=/tmp/chrome"
     ]
   },
 
--- packages/dsl/testem.js
+++ packages/dsl/testem.js
@@ -16,7 +16,7 @@ module.exports = {
       "--no-sandbox",
       "--disable-gpu",
       "--remote-debugging-port=9222",
-      "--user-data-dir=/tmp"
+      "--user-data-dir=/tmp/chrome"
     ]
   },
 
